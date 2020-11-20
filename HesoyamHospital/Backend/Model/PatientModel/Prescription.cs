6db69a9198b05a22f089663a4267701ad79fb485
= <rankovictamaraa@gmail.com>
2020-11-20 16:02:18 +0100

    refactor: document model changed

--- HesoyamHospital/Backend/Model/PatientModel/Prescription.cs
+++ HesoyamHospital/Backend/Model/PatientModel/Prescription.cs
@@ -10,34 +10,10 @@ using System;
 
 namespace Backend.Model.PatientModel
 {
-    public class Prescription : IIdentifiable<long>
+    public class Prescription : Document, IIdentifiable<long>
     {
-        private long _id;
-        public long Id { get => _id; set => _id = value; }
-
         private PrescriptionStatus _status;
         public PrescriptionStatus Status { get => _status; set => _status = value; }
-
-        private DateTime _dateCreated;
-        public DateTime DateCreated { get => _dateCreated; set => _dateCreated = value; }
-
-        private Patient _patient;
-        public Patient Patient { get => _patient; set { _patient = value; _patientID = value.Id; } }
-
-        private long _patientID;
-        public long PatientID { get => _patientID; set => _patientID = value; }
-
-        private Doctor _doctor;
-        public Doctor Doctor { get => _doctor; set { _doctor = value; _doctorID = value.Id; } }
-
-        private long _doctorID;
-        public long DoctorID { get => _doctorID; set => _doctorID = value; }
-
-        private Diagnosis _diagnosis;
-        public Diagnosis Diagnosis { get => _diagnosis; set { _diagnosis = value; _diagnosisID = value.Id; } }
-
-        private long _diagnosisID;
-        public long DiagnosisID { get => _diagnosisID; set => _diagnosisID = value; }
 
         private List<MedicalTherapy> _medicalTherapies;
         public List<MedicalTherapy> MedicalTherapies
--- HesoyamHospital/Backend/Model/PatientModel/Report.cs
+++ HesoyamHospital/Backend/Model/PatientModel/Report.cs
@@ -3,39 +3,15 @@ using Backend.Repository.Abstract;
 using System;
 namespace Backend.Model.PatientModel
 {
-    public class Report : IIdentifiable<long>
+    public class Report : Document, IIdentifiable<long>
     {
-        private long _id;
-        public long Id { get => _id; set => _id = value; }
-
-        private Patient _patient;
-        public Patient Patient { get => _patient; set { _patient = value; _patientID = value.Id; } }
-
-        private long _patientID;
-        public long PatientID { get => _patientID; set => _patientID = value; }
-
-        private Doctor _doctor;
-        public Doctor Doctor { get => _doctor; set { _doctor = value; _doctorID = value.Id; } }
-
-        private long _doctorID;
-        public long DoctorID { get => _doctorID; set => _doctorID = value; }
-
-        private DateTime _dateTimeCreated;
-        public DateTime DateTimeCreated { get => _dateTimeCreated; set => _dateTimeCreated = value; }
-
         private string _comment;
         public string Comment { get => _comment; set => _comment = value; }
-
-        private Diagnosis _diagnosis;
-        public Diagnosis Diagnosis { get => _diagnosis; set { _diagnosis = value; _diagnosisID = value.Id; } }
-
-        private long _diagnosisID;
-        public long DiagnosisID { get => _diagnosisID; set => _diagnosisID = value; }
 
         public Report (long id)
         {
             _id = id;
-            DateTimeCreated = DateTime.Now;
+            _dateCreated = DateTime.Now;
         }
 
         public Report (long id, Patient patient, Doctor doctor, string comment, Diagnosis diagnosis)
