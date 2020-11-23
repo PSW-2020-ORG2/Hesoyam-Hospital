6aed2c1d2085d5db9eccd7183bdab1633211cac3
= <rankovictamaraa@gmail.com>
2020-11-23 14:42:26 +0100

    feat: check if search criteria is valid

---
+++ HesoyamHospital/WebApplication/Documents/DocumentsValidation.cs
@@ -0,0 +1,22 @@
+﻿using Backend.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Documents
+{
+    public class DocumentsValidation
+    {
+        public bool isSearchCriteriaValid(DocumentSearchCriteria criteria)
+            => criteria != null &&
+            criteria.DoctorName != null &&
+            criteria.MedicineName != null &&
+            criteria.Comment != null &&
+            criteria.DiagnosisName != null &&
+            criteria.TimeInterval != null &&
+            criteria.TimeInterval.isFullyDefined() &&
+            criteria.TimeInterval.isInOrder() &&
+            criteria.TimeInterval.isInThePast();
+    }
+}
