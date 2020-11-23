e8a3e92beaa079abd3fb137567c71dd3b2b53822
= <rankovictamaraa@gmail.com>
2020-11-23 14:44:40 +0100

    feat: search result objects to be presented to the user

---
+++ HesoyamHospital/WebApplication/Documents/DocumentsMapper.cs
@@ -0,0 +1,16 @@
+﻿using Backend.Model.PatientModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Documents
+{
+    public class DocumentsMapper
+    {
+        public static DocumentDTO DocumentToDocumentDTO(Document document)
+        {
+            return new DocumentDTO(document.DateCreated, document.Doctor.FullName, document.Diagnosis.diagnosisName);
+        }
+    }
+}
