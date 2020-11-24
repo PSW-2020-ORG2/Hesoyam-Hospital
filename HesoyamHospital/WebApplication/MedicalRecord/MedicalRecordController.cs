e12bd691ccad020a12277eb41d2ae1784e3ff81f
anja-meseldzic <aanja.meseldzic@gmail.com>
2020-11-24 16:03:44 +0100

    feat: medical record controller logic

---
+++ HesoyamHospital/WebApplication/MedicalRecord/MedicalRecordController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Backend;
+using Backend.Model.PatientModel;
+using Backend.Model.UserModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.MedicalRecords
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MedicalRecordController : ControllerBase
+    {
+        [HttpGet("show")]  //GET /api/medicalrecord/show
+        public IActionResult GetMedicalRecordByPatientId()
+        {
+            MedicalRecord medicalRecord = AppResources.getInstance().medicalRecordService.GetPatientMedicalRecordByPatientId(500);
+
+            if (medicalRecord == null) return NotFound();
+
+            return Ok(MedicalRecordMapper.MedicalRecordToMedicalRecordDTO(medicalRecord));
+        }
+    }
+}
---
+++ HesoyamHospital/WebApplication/MedicalRecord/MedicalRecordMapper.cs
@@ -0,0 +1,50 @@
+﻿using Backend.Model.PatientModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.MedicalRecords
+{
+    public class MedicalRecordMapper
+    {
+        public static MedicalRecordDTO MedicalRecordToMedicalRecordDTO(MedicalRecord medicalRecord)
+        {
+            MedicalRecordDTO dto = new MedicalRecordDTO
+            {
+                FirstName = medicalRecord.Patient.Name,
+                LastName = medicalRecord.Patient.Surname,
+                MiddleName = medicalRecord.Patient.MiddleName,
+                //Address = medicalRecord.Patient.Address.ToString(),
+                DateOfBirth = medicalRecord.Patient.DateOfBirth.ToString(),
+                MedicalId = medicalRecord.Patient.HealthCardNumber,
+                PersonalId = medicalRecord.Patient.Jmbg,
+                MobilePhone = medicalRecord.Patient.CellPhone,
+                HomePhone = medicalRecord.Patient.HomePhone,
+                Email = medicalRecord.Patient.Email1,
+                Username = medicalRecord.Patient.UserName,
+                Password = medicalRecord.Patient.Password,
+                BloodType = medicalRecord.BloodTypeToString(medicalRecord.PatientBloodType),
+                Alergies = medicalRecord.AllergiesListToString(medicalRecord.Allergy),
+                Prescriptions = PrescriptionListToPrescriptionDTOList(medicalRecord.Prescriptions),
+
+            };
+
+            return dto;
+        }
+
+        public static List<PrescriptionDTO> PrescriptionListToPrescriptionDTOList(List<Prescription> prescriptions) {
+            List<PrescriptionDTO> prescriptionDTOs = new List<PrescriptionDTO>();
+            List<string> medicine = new List<string>();
+
+            foreach (Prescription p in prescriptions) {
+                foreach (MedicalTherapy mt in p.MedicalTherapies) {
+                    medicine.Add(mt.Medicine.Name);
+                }
+                PrescriptionDTO prescDto = new PrescriptionDTO(medicine, p.StatusToString(p.Status));
+                prescriptionDTOs.Add(prescDto);
+            }
+            return prescriptionDTOs;
+        }
+    }
+}
