89d5fbe75d7f7af86c51dd94659fcfaa0a1100c2
Milijana <milijana.djordjevic1998@gmail.com>
2020-11-27 23:53:20 +0100

    feat: send email service for account activation

---
+++ HesoyamHospital/WebApplication/Authentication/SendEmail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Authentication
+{
+    public class SendEmail : ISendEmail
+    {
+        public SendEmail() {}
+
+        public void SendActivationEmail(long id)
+        {
+            return;
+        }
+    }
+}
