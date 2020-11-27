8802dce84913543f31a651094ed9f9b21830bb13
turkovic98 <eminaturkovic600@gmail.com>
2020-11-27 12:55:44 +0100

    test: calculating average grade per each doctor test created

--- HesoyamHospital/WebApplicationTests/Integration/HospitalSurvey/GetAnswersTests.cs
+++ HesoyamHospital/WebApplicationTests/Integration/HospitalSurvey/GetAnswersTests.cs
@@ -42,6 +42,17 @@ namespace WebApplicationTests.Integration.HospitalSurvey
 
             response.StatusCode.ShouldBeEquivalentTo(expectedStatusCode);
         }
+
+        [Theory]
+        [MemberData(nameof(Data2))]
+        public async void Getting_Average_Grade_Per_Each_Doctor_Status_Code_Tests(int id, HttpStatusCode expectedStatusCode)
+        {
+            HttpClient client = _factory.CreateClient();
+
+            HttpResponseMessage response = await client.GetAsync("/api/survey/average-grade-per-doctor/" + id);
+
+            response.StatusCode.ShouldBeEquivalentTo(expectedStatusCode);
+        }
         [Theory]
         [MemberData(nameof(Data1))]
         public async void Mean_Values_Per_Section_Status_Code_Tests(string section, HttpStatusCode expectedStatusCode)
