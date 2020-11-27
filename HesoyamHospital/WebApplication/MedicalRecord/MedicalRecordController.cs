c563bcbe6d433f3b39e90e45d488d318ac0b5134
anja-meseldzic <aanja.meseldzic@gmail.com>
2020-11-27 14:38:21 +0100

    feat: show image controller method

--- HesoyamHospital/WebApplication/MedicalRecord/MedicalRecordController.cs
+++ HesoyamHospital/WebApplication/MedicalRecord/MedicalRecordController.cs
@@ -22,5 +22,19 @@ namespace WebApplication.MedicalRecords
 
             return Ok(MedicalRecordMapper.MedicalRecordToMedicalRecordDTO(medicalRecord));
         }
+
+        [HttpGet("image/{imageName}")]
+        public IActionResult GetImage(String imageName)
+        {
+            Byte[] b;
+            try
+            {
+                b = System.IO.File.ReadAllBytes(@"../WebApplication/Resources/images/" + imageName);
+            }
+            catch (Exception e) {
+                b = System.IO.File.ReadAllBytes(@"../WebApplication/Resources/images/anjaa.jpg");
+            }
+            return File(b, "image/jpg");
+        }
     }
 }
