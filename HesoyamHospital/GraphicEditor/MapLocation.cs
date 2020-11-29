2d67e70dadc0dd63e1173c4452c5010818679d78
milica1612 <57577588+milica1612@users.noreply.github.com>
2020-11-29 22:51:52 +0100

    feat: search objects on the map
    Wpf, finding objects on the map and displaying results
    
    Co-Authored-By: ivanagavric98 <65288879+ivanagavric98@users.noreply.github.com>

---
+++ HesoyamHospital/GraphicEditor/MapLocation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraphicEditor
+{
+    public class MapLocation
+    {
+        
+        public string Name { get; set; }
+
+        public string Hospital { get; set; }
+
+        public string Floor { get; set; }
+
+        public MapLocation() 
+        { 
+        }
+
+        public MapLocation(string hospital, string floor, string name)
+        {
+            Name = name;
+            Hospital = hospital;
+            Floor = floor;
+
+        }
+
+        
+ 
+    }
+}
---
+++ HesoyamHospital/GraphicEditor/SearchService.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Internal;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace GraphicEditor
+{
+    class SearchService
+    {
+        public SearchService()
+        {
+        }
+
+        public List<MapLocation> Find_objects_by_name(string name) 
+        {
+            List<MapLocation> map_locations = new List<MapLocation>();
+            List<MapLocation> map_location1 = Find_in_file("hospital1groundfloor.txt", name);
+            List<MapLocation> map_location2 = Find_in_file("hospital1firstfloor.txt", name);
+            List<MapLocation> map_location3 = Find_in_file("hospital2groundfloor.txt", name);
+            List<MapLocation> map_location4 = Find_in_file("hospital2firstfloor.txt", name);
+            List<MapLocation> map_location5 = Find_in_file("hospital2secondfloor.txt", name);
+
+            map_locations.AddRange(map_location1);
+            
+            map_locations.AddRange(map_location2);
+
+            map_locations.AddRange(map_location3);
+            
+            map_locations.AddRange(map_location4);
+            
+            map_locations.AddRange(map_location5);
+
+            return map_locations;
+        }
+
+        public List<MapLocation> Find_in_file(string path, string name)
+        {
+            List<String> names = new List<string>();
+            MapLocation map_location = null ;
+            List<MapLocation> map_locations = new List<MapLocation>();
+            GraphicRepository graphic_repository = new GraphicRepository();
+            List<GraphicalObject> graphical_objects = graphic_repository.ReadFromFile(path);
+
+            foreach (GraphicalObject obj in graphical_objects) {
+                if (obj.Name.Contains(name)) 
+                {
+                    string full_name = obj.Name;
+                    map_location = Get_Location_By_Filename(path, full_name);
+                    map_locations.Add(map_location);
+                    
+                }
+            }
+
+            return map_locations;
+        
+        }
+
+        public MapLocation Get_Location_By_Filename(string path, string name) {
+            MapLocation map_location = new MapLocation();
+
+            map_location.Name = name;
+            if (path.Contains("1"))
+            {
+                map_location.Hospital = "Hospital 1";
+            }
+            if (path.Contains("2"))
+            {
+                map_location.Hospital = "Hospital 2";
+            }
+            if (path.Contains("first"))
+            { 
+                map_location.Floor = "First Floor";
+            }
+            if (path.Contains("second"))
+            {
+                map_location.Floor = "Second Floor";
+            }
+            if (path.Contains("ground"))
+            {
+                map_location.Floor = "Ground Floor";
+            }
+
+            return map_location;
+        }
+
+    }
+}
