3ea4b2ec92a7fef9c29d10f13fa209f767d0b345
Milijana <milijana.djordjevic1998@gmail.com>
2020-11-29 13:05:04 +0100

    fix: function parameter changed

--- HesoyamHospital/WebApplication/Authentication/ISendEmail.cs
+++ HesoyamHospital/WebApplication/Authentication/ISendEmail.cs
@@ -7,7 +7,7 @@ namespace WebApplication.Authentication
 {
     public interface ISendEmail
     {
-        public void SendActivationEmail(long id, string email);
+        public void SendActivationEmail(long id, string patientEmailAddress);
         public long TokenToId(string token);
     }
 }
