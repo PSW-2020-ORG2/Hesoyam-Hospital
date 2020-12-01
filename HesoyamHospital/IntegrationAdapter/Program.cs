be38a74357f9bc23288730190296e286da9dff62
RemaxX7 <marko.rakic98@gmail.com>
2020-12-01 02:57:13 +0000

    fix: revert to previous commit

--- HesoyamHospital/IntegrationAdapter/Program.cs
+++ HesoyamHospital/IntegrationAdapter/Program.cs
@@ -11,7 +11,7 @@ namespace IntegrationAdapter
 {
     public class Program
     {
-        public ICollection<ActionBenefit> NewsMessages = new List<ActionBenefit>(); 
+        public static readonly ICollection<ActionBenefit> NewsMessages = new List<ActionBenefit>(); 
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
