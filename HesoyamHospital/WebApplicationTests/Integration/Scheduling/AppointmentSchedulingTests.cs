f4bf24a3d61660d94cf4036b0bd8460c52121d40
turkovic98 <eminaturkovic600@gmail.com>
2020-12-02 19:22:44 +0100

    test: Unit and integration tests for scheduling created

---
+++ HesoyamHospital/WebApplicationTests/Integration/Scheduling/AppointmentSchedulingTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+using WebApplication;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using Shouldly;
+using WebApplication.Scheduling;
+using System;
+using Newtonsoft.Json;
+
+namespace WebApplicationTests.Integration.Scheduling
+{
+    public class AppointmentSchedulingTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public AppointmentSchedulingTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [MemberData(nameof(DoctorTypeData))]
+        public async void Getting_all_doctors_for_selected_type(string type, HttpStatusCode expectedStatusCode)
+        {
+            HttpClient client = _factory.CreateClient();
+
+            HttpResponseMessage response = await client.GetAsync("/api/appointment/getDoctorsByType/" + type);
+
+            response.StatusCode.ShouldBeEquivalentTo(expectedStatusCode);
+        }
+
+        [Theory]
+        [MemberData(nameof(DoctorTimeData))]
+        public async void Getting_all_times_for_selected_date_and_doctor(DoctorDateDTO dto, HttpStatusCode expectedStatusCode)
+        {
+            HttpClient client = _factory.CreateClient();
+            StringContent bodyContent = new StringContent(JsonConvert.SerializeObject(dto), System.Text.Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await client.PostAsync("/api/appointment/getTimesForDoctor", bodyContent);
+
+            response.StatusCode.ShouldBeEquivalentTo(expectedStatusCode);
+        }
+
+
+        public static IEnumerable<object[]> DoctorTypeData =>
+            new List<object[]>
+            {
+                new object[] { "GENERAL_PRACTITIONER", HttpStatusCode.OK },
+                new object[] { "GENERAL_DOCTOR", HttpStatusCode.BadRequest },
+            };
+
+
+        public static IEnumerable<object[]> DoctorTimeData =>
+            new List<object[]>
+            {
+                new object[] { new DoctorDateDTO(10, DateTime.Now), HttpStatusCode.OK },
+                new object[] { null, HttpStatusCode.BadRequest },
+            };
+    }
+}
---
+++ HesoyamHospital/WebApplicationTests/Unit/Scheduling/AppointmentSchedulingTests.cs
@@ -0,0 +1,33 @@
+﻿using Backend.Model.PatientModel;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using WebApplication.Scheduling;
+using WebApplication.Scheduling.Service;
+using Xunit;
+
+namespace WebApplicationTests.Unit.Scheduling
+{
+    public class AppointmentSchedulingTests
+    {
+        [Theory]
+        [MemberData(nameof(AppointmentData))]
+        public void Saving_appointment_to_database(AppointmentDTO dto, Times times)
+        {
+            var appointmentService = new Mock<IAppointmentSchedulingService>();
+            var controller = new AppointmentController(appointmentService.Object);
+
+            controller.SaveAppointment(dto);
+
+            appointmentService.Verify(n => n.SaveAppointment(It.IsAny<Appointment>()), times);
+        }
+        public static IEnumerable<object[]> AppointmentData =>
+           new List<object[]>
+           {
+                new object[] { new AppointmentDTO(), Times.Once()},
+                new object[] { null, Times.Never() },
+           };
+    }
+}
