cd6e28f85420e1c66b1e387b48fe2220091e65d5
Milijana <milijana.djordjevic1998@gmail.com>
2020-12-02 14:59:26 +0100

    feat: backend methods for changing the selected doctor

---
+++ HesoyamHospital/WebApplication/MedicalRecords/DoctorMapper.cs
@@ -0,0 +1,26 @@
+﻿using Backend.Model.UserModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.MedicalRecords
+{
+    public static class DoctorMapper
+    {
+        public static DoctorDTO DoctorToDoctorDTO(Doctor doctor)
+        {
+            return (new DoctorDTO(doctor.Id, doctor.FullName));
+        }
+
+        public static List<DoctorDTO> DoctorListToDTOList(List<Doctor> doctors)
+        {
+            List<DoctorDTO> dtos = new List<DoctorDTO>();
+            foreach (Doctor doctor in doctors)
+            {
+                dtos.Add(DoctorToDoctorDTO(doctor));
+            }
+            return dtos;
+        }
+    }
+}
---
+++ HesoyamHospital/WebApplication/MedicalRecords/MedicalRecordController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Backend;
+using Backend.Model.DoctorModel;
+using Backend.Model.PatientModel;
+using Backend.Model.UserModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.MedicalRecords
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MedicalRecordController : ControllerBase
+    {
+        [HttpGet("show/{id}")]  //GET /api/medicalrecord/show/500
+        public IActionResult GetMedicalRecordByPatientId(long id)
+        {
+            Backend.Model.PatientModel.MedicalRecord medicalRecord = AppResources.getInstance().medicalRecordService.GetPatientMedicalRecordByPatientId(id);
+
+            if (medicalRecord == null) return NotFound();
+
+            return Ok(MedicalRecordMapper.MedicalRecordToMedicalRecordDTO(medicalRecord));
+        }
+
+        [HttpGet("allGeneralDoctors")]
+        public IActionResult GetDoctors()
+        {
+            List<Doctor> doctors = AppResources.getInstance().doctorService.GetDoctorByType(DoctorType.GENERAL_PRACTITIONER).ToList();
+            if (doctors == null) return NotFound();
+            List<DoctorDTO> dtos = DoctorMapper.DoctorListToDTOList(doctors);
+            return Ok(dtos.ToArray());
+        }
+
+        [HttpPost("changeSelectedDoctor")]  //POST /api/medicalrecord/changeSelectedDoctor
+        public IActionResult ChangeSelectedDoctor(SelectedDoctorDTO newDoctor)
+        {
+            Patient patient = AppResources.getInstance().patientService.GetByUsername(newDoctor.Username);
+            if (patient == null) return NotFound();
+            patient = AppResources.getInstance().patientService.ChangeSelectedDoctor(newDoctor.DoctorId, patient.Id);
+            if (patient == null) return BadRequest();
+            return Ok();
+        }
+
+    }
+}
