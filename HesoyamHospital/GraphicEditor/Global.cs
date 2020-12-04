ca045306fb625e47fd99c4e59508917d99483af4
milica1612 <57577588+milica1612@users.noreply.github.com>
2020-12-05 00:42:16 +0100

    feat: patient restriction
    Preventing patient from reading information about equipment and medicine.

--- HesoyamHospital/GraphicEditor/Global.cs
+++ HesoyamHospital/GraphicEditor/Global.cs
@@ -4,7 +4,7 @@ namespace GraphicEditor
     public static class Global
     {
         public static string SearchObjectName { get; set; }
-
+        public static string LoggedInUserType { get; set; }
 
     }
 }
