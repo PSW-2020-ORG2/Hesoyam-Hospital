d0fb2d6e4d306375c3b7dfb45025d16940ea985b
Milijana <milijana.djordjevic1998@gmail.com>
2020-12-05 15:57:13 +0100

    feat: dto and dto mapper for recommended times

---
+++ HesoyamHospital/WebApplication/Scheduling/PriorityIntervalDTO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Scheduling
+{
+    public class PriorityIntervalDTO
+    {
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string StartTimeText { get; set; }
+        public string DateText { get; set; }
+        public long DoctorId { get; set; }
+        public string FullName { get; set; }
+
+        public PriorityIntervalDTO() { }
+
+        public PriorityIntervalDTO(DateTime startTime, DateTime endTime, long doctorId, string fullName)
+        {
+            StartTime = startTime;
+            EndTime = endTime;
+            StartTimeText = startTime.ToString("HH:mm") + " - " + endTime.ToString("HH:mm");
+            DateText = startTime.ToString("dd.MM.yyyy");
+            DoctorId = doctorId;
+            FullName = fullName;
+        }
+    }
+}
---
+++ HesoyamHospital/WebApplication/Scheduling/PriorityIntervalMapper.cs
@@ -0,0 +1,26 @@
+﻿using Backend.Model.UserModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Scheduling
+{
+    public static class PriorityIntervalMapper
+    {
+        public static PriorityIntervalDTO ToDto(Doctor doctor, DateTime dateTime)
+        {
+            return new PriorityIntervalDTO(dateTime, dateTime.AddMinutes(AppointmentMapper.AppointmentDurationMinutes), doctor.Id, doctor.FullName);
+        }
+
+        public static List<PriorityIntervalDTO> ListToDtoListForOneDoctor(Doctor doctor, List<DateTime> dateTimes)
+        {
+            List<PriorityIntervalDTO> dtos = new List<PriorityIntervalDTO>();
+            foreach (DateTime dateTime in dateTimes)
+            {
+                dtos.Add(ToDto(doctor, dateTime));
+            }
+            return dtos;
+        }
+    }
+}
