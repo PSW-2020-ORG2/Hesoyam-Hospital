281191d058d15e29dfb95ce80d855b05cc8ec010
= <rankovictamaraa@gmail.com>
2020-12-06 15:07:35 +0100

    test: unit tests written

---
+++ HesoyamHospital/WebApplicationTests/Unit/Appointments/CancelAppointmentsTests.cs
@@ -0,0 +1,60 @@
+﻿using Backend.Model.PatientModel;
+using Backend.Model.UserModel;
+using Backend.Repository.Abstract.MedicalAbstractRepository;
+using Backend.Repository.Abstract.UsersAbstractRepository;
+using Moq;
+using Shouldly;
+using System.Collections.Generic;
+using WebApplication.Appointments.Service;
+using Xunit;
+
+namespace WebApplicationTests.Unit.Appointments
+{
+    public class CancelAppointmentsTests
+    {
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void Cancel_appointment(long patientId, long appointmentId, bool canceledStatus)
+        {
+            AppointmentService service = new AppointmentService(CreatePatientStubRepository(), CreateAppointmentStubRepository());
+
+            service.Cancel(patientId, appointmentId);
+
+            service.GetByID(appointmentId).Canceled.ShouldBe(canceledStatus);
+        }
+
+        private static IPatientRepository CreatePatientStubRepository()
+        {
+            var stubRepository = new Mock<IPatientRepository>();
+
+            Patient patient = new Patient(0);
+            patient.Appointments = new List<Appointment>();
+            patient.Appointments.Add(appointment);
+
+            stubRepository.Setup(r => r.GetByID(0)).Returns(patient);
+
+            return stubRepository.Object;
+        }
+
+        private static IAppointmentRepository CreateAppointmentStubRepository()
+        {
+            var stubRepository = new Mock<IAppointmentRepository>();
+
+            stubRepository.Setup(r => r.GetByID(0)).Returns(appointment);
+
+            return stubRepository.Object;
+        }
+
+        public static IEnumerable<object[]> Data =>
+        new List<object[]>
+        {
+            new object[] { 0, 0, true },
+        };
+
+        public static Appointment appointment =>
+        new Appointment()
+        {
+            Canceled = false
+        };
+    }
+}
