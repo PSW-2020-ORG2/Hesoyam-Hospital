e2859be6a615c01f84c1f188e07e70ea5d4d57dc
= <rankovictamaraa@gmail.com>
2020-12-06 14:08:26 +0100

    test: integration test written

---
+++ HesoyamHospital/WebApplicationTests/Integration/Appointments/CancelAppointmentsTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Shouldly;
+using System.Net;
+using System.Net.Http;
+using WebApplication;
+using Xunit;
+
+namespace WebApplicationTests.Integration.Appointments
+{
+    public class CancelAppointmentsTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public CancelAppointmentsTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async void Cancel_appointment()
+        {
+            HttpClient client = _factory.CreateClient();
+
+            HttpResponseMessage response = await client.PutAsync("/api/appointment/cancel", new StringContent("1"));
+
+            HttpStatusCode[] possibleStatusCodes = { HttpStatusCode.OK, HttpStatusCode.NotFound};
+            response.StatusCode.ShouldBeOneOf(possibleStatusCodes);
+        }
+    }
+}
