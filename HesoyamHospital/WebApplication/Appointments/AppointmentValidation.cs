8c130af811505be59943f06e427386c826ccb8c3
= <rankovictamaraa@gmail.com>
2020-12-06 15:06:23 +0100

    feat: only certain appointments can be cancelled

---
+++ HesoyamHospital/WebApplication/Appointments/AppointmentValidation.cs
@@ -0,0 +1,16 @@
+﻿using Backend.Model.PatientModel;
+using System;
+
+namespace WebApplication.Appointments
+{
+    public class AppointmentValidation
+    {
+        private readonly int hoursToCancelBeforeAppointment = 48;
+        public bool IsPossibleToCancelAppointment(Appointment appointment, long patientId)
+            => !appointment.TimeInterval.IsInThePast() 
+            && !appointment.TimeInterval.IsDateTimeBetween(DateTime.Now) 
+            && !appointment.Canceled 
+            && appointment.Patient.Id == patientId 
+            && appointment.TimeInterval.EndTime.AddHours(hoursToCancelBeforeAppointment) < DateTime.Now;
+    }
+}
