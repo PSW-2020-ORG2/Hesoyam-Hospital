4f0cba11ec069239165f11d0971709cfbeb65653
Milijana <milijana.djordjevic1998@gmail.com>
2020-12-09 23:12:27 +0100

    feat: suspicious patients dto and mapper

---
+++ HesoyamHospital/WebApplication/Appointments/DTOs/BlockPatientDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Appointments.DTOs
+{
+    public class BlockPatientDTO
+    {
+        public string Username { get; set; }
+        public int CancelCount { get; set; }
+        public string FullName { get; set; }
+
+        public BlockPatientDTO(string username, int cancelCount, string fullName)
+        {
+            Username = username;
+            CancelCount = cancelCount;
+            FullName = fullName;
+        }
+    }
+}
