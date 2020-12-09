cbf94cddc3a09eb5564e9152f8460f90c00e186e
Milijana <milijana.djordjevic1998@gmail.com>
2020-12-09 23:10:14 +0100

    feat: get patients that cancelled appointments in previous month

--- HesoyamHospital/Backend/Repository/Abstract/MedicalAbstractRepository/ICancellationRepository.cs
+++ HesoyamHospital/Backend/Repository/Abstract/MedicalAbstractRepository/ICancellationRepository.cs
@@ -1,8 +1,10 @@
 ﻿using Backend.Model.PatientModel;
+using System.Collections.Generic;
 
 namespace Backend.Repository.Abstract.MedicalAbstractRepository
 {
     public interface ICancellationRepository : IRepository<Cancellation, long>
     {
+        public Dictionary<long, int> GetCancelledCountForPatients();
     }
 }
