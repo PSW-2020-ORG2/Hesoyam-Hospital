806c83899ea57aaa1e555d5df25746354f8ac748
RemaxX7 <marko.rakic98@gmail.com>
2020-12-13 17:24:14 +0000

    fix: removed absolute paths

--- HesoyamHospital/IntegrationAdapter/Controllers/MedicineSpecificationAcquisitionController.cs
+++ HesoyamHospital/IntegrationAdapter/Controllers/MedicineSpecificationAcquisitionController.cs
@@ -31,15 +31,17 @@ namespace IntegrationAdapter.Controllers
         [HttpGet("specification/{name}")]
         public IActionResult GetSpecification(string name)
         {
-            string text = System.IO.File.ReadAllText(@"D:\Users\Marko\Desktop\RebexTinySftpServer-Binaries-Latest\data\specifications\"+name+".txt");
+            string startupPath = Directory.GetCurrentDirectory();
+            string text = System.IO.File.ReadAllText(startupPath + @"\PrescribedMedicineReport\specifications\"+name+".txt");
             return Ok(text);
         }
         [HttpPut("prescription")]
         public IActionResult CreatePrescriptionFile(long id,string text)
         {
             //Patient patient = AppResources.getInstance().patientService.GetByID(id);
-            string filepath = @"D:\Users\Marko\Desktop\RebexTinySftpServer-Binaries-Latest\data\prescriptions\" + id +"_"+ DateTime.Now.Hour +"-" + DateTime.Now.Minute + ".txt";   //id promeniti na patient.Name
-            using (StreamWriter sw = System.IO.File.CreateText(filepath))
+            string startupPath = Directory.GetCurrentDirectory();
+            string filepath = @"\PrescribedMedicineReport\prescriptions\" + id +"_"+ DateTime.Now.Hour +"-" + DateTime.Now.Minute + ".txt";   //id promeniti na patient.Name
+            using (StreamWriter sw = System.IO.File.CreateText(startupPath + filepath))
             {
                 sw.WriteLine(text);
             }
