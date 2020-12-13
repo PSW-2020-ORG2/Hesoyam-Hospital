fea18646e7b5e0e2c987553b5d3b7e77f510c05c
lukapetrovic3110 <luka.petrovic@uns.ac.rs>
2020-12-13 22:49:44 +0100

    feat: added MedicineDTO

--- HesoyamHospital/GraphicEditor/DTOs/MedicineDTO.cs
+++ HesoyamHospital/GraphicEditor/DTOs/MedicineDTO.cs
@@ -1,18 +1,17 @@
 ﻿
 namespace GraphicEditor
 {
-    class EquipmentAndMedicineDTO
+    class MedicineDTO
     {
-        public string Type { get; set; }
         public string Name { get; set; }
+        public string Type { get; set; }
         public long Room { get; set; }
         public int Quantity { get; set; }
 
-
-        public EquipmentAndMedicineDTO(string type, string name, long room, int quantity)
+        public MedicineDTO(string name, string type, long room, int quantity)
         {
-            Type = type;
             Name = name;
+            Type = type;
             Room = room;
             Quantity = quantity;
         }
