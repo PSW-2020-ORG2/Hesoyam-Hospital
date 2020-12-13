4f533be117425e7899c1bbd6be45c1d6a8b23abf
milica1612 <57577588+milica1612@users.noreply.github.com>
2020-12-13 22:12:38 +0100

    refactor: refactor DTO from medicine and equipment

--- HesoyamHospital/GraphicEditor/DTOs/EquipmentAndMedicineDTO.cs
+++ HesoyamHospital/GraphicEditor/DTOs/EquipmentAndMedicineDTO.cs
@@ -5,22 +5,14 @@ namespace GraphicEditor
     {
         public string Type { get; set; }
         public string Name { get; set; }
-
-        public string MapObject { get; set; }
-
-        public string Floor { get; set; }
-
-        public string Room { get; set; }
-
+        public long Room { get; set; }
         public int Quantity { get; set; }
 
 
-        public EquipmentAndMedicineDTO(string type, string name, string mapObject, string floor, string room, int quantity)
+        public EquipmentAndMedicineDTO(string type, string name, long room, int quantity)
         {
             Type = type;
             Name = name;
-            MapObject = mapObject;
-            Floor = floor;
             Room = room;
             Quantity = quantity;
         }
