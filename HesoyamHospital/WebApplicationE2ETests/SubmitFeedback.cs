0a1bf83f9a9c2c6337c10dae390703eef0e2b6a6
LukaKrickovic <krickovicluka@gmail.com>
2020-12-14 00:00:15 +0100

    chore: fixed docker-compose ports

---
+++ HesoyamHospital/WebApplicationE2ETests/SubmitFeedback.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationE2ETests
+{
+    public class SubmitFeedback
+    {
+        private IWebDriver driver;
+        public const string URI = "http://localhost:52166/feedback/patient/post";
+
+        public SubmitFeedback()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("start-maximized");            // open Browser in maximized mode
+            options.AddArguments("disable-infobars");           // disabling infobars
+            options.AddArguments("--disable-extensions");       // disabling extensions
+            options.AddArguments("--disable-gpu");              // applicable to windows os only
+            options.AddArguments("--disable-dev-shm-usage");    // overcome limited resource problems
+            options.AddArguments("--no-sandbox");               // Bypass OS security model
+            options.AddArguments("--disable-notifications");    // disable notifications
+
+            driver = new ChromeDriver(options);
+        }
+    }
+}
