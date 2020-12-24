6f2e3f030bad017a8b5a0214642ff76afc865970
Milijana <milijana.djordjevic1998@gmail.com>
2020-12-24 15:26:45 +0100

    refactor: appointment microservice created
    repository base created

---
+++ HesoyamHospital/Appointment/Repository/Abstract/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Appointment.Repository.Abstract
+{
+    public interface IRepository<T, ID>
+    {
+        IEnumerable<T> GetAll();
+
+        T GetByID(ID id);
+
+        T Create(T entity);
+
+        void Update(T entity);
+
+        void UpdateProperty(T entity, string propertyName);
+
+        void Delete(T entity);
+
+    }
+}
