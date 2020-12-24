57d421836e8f63e7fa03135b694ef3555fb2adcd
Milijana <milijana.djordjevic1998@gmail.com>
2020-12-24 15:29:03 +0100

    refactor: documents microservice created
    repository base created

---
+++ HesoyamHospital/Documents/Repository/SQLRepository/Base/ISQLStream.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Documents.Repository.SQLRepository.Base
+{
+    public interface ISQLStream<T>
+    {
+        void SaveAll();
+
+        IEnumerable<T> ReadAll();
+
+        IEnumerable<T> ReadAllEager();
+
+        void Append(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+
+        void UpdateProperty(T entity, string propertyName);
+    }
+}
