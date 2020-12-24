2817330e886d392e0a461c2ca723e21f0ef06250
= <rankovictamaraa@gmail.com>
2020-12-24 22:21:07 +0100

    test: end-to-end test written

---
+++ HesoyamHospital/WebApplicationTests/EndToEnd/PostFeedbackTests.cs
@@ -0,0 +1,171 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Shouldly;
+using System;
+using WebApplicationTests.EndToEnd.Pages;
+using Xunit;
+
+namespace WebApplicationTests.EndToEnd
+{
+    public class PostFeedbackTests : IDisposable
+    {
+        private IWebDriver driver;
+        private PostFeedback postFeedbackPage;
+        private FeedbackPublishList feedbackPublishListPage;
+        private int feedbackCount;
+
+        public PostFeedbackTests()
+        {
+            InitializeDriver();
+
+            postFeedbackPage = new PostFeedback(driver);
+            feedbackPublishListPage = new FeedbackPublishList(driver);
+
+            feedbackPublishListPage.Navigate();
+            //feedbackPublishListPage.EnsurePageIsDisplayed();
+            feedbackPublishListPage.URI.ShouldBeEquivalentTo(driver.Url);
+            feedbackCount = feedbackPublishListPage.FeedbackCount();
+
+            postFeedbackPage.Navigate();
+            postFeedbackPage.EnsurePageIsDisplayed();
+            postFeedbackPage.URI.ShouldBeEquivalentTo(driver.Url);
+        }
+
+        [Fact]
+        public void Successful_public_anonymous_feedback()
+        {
+            string feedback = "Awesome!";
+            postFeedbackPage.InsertFeedback(feedback);
+            postFeedbackPage.SelectPublicRadioButton();
+            postFeedbackPage.SelectAnonymousRadioButton();
+            postFeedbackPage.SubmitFeedback();
+
+            feedbackPublishListPage.Navigate();
+            feedbackPublishListPage.EnsurePageIsDisplayed();
+
+            feedbackPublishListPage.GetLastRowUsername().ShouldBeEquivalentTo("Anonymous");
+            feedbackCount.ShouldBe(feedbackPublishListPage.FeedbackCount() - 1);
+            feedbackPublishListPage.GetLastRowFeedback().ShouldBe(feedback);
+        }
+
+        [Fact]
+        public void Successful_private_anonymous_feedback()
+        {
+            string feedback = "Awesome!";
+            postFeedbackPage.InsertFeedback(feedback);
+            postFeedbackPage.SelectPrivateRadioButton();
+            postFeedbackPage.SelectAnonymousRadioButton();
+            postFeedbackPage.SubmitFeedback();
+
+            feedbackPublishListPage.Navigate();
+            feedbackPublishListPage.EnsurePageIsDisplayed();
+
+            feedbackPublishListPage.GetLastRowUsername().ShouldBeEquivalentTo("Anonymous");
+            feedbackCount.ShouldBe(feedbackPublishListPage.FeedbackCount() - 1);
+            feedbackPublishListPage.GetLastRowFeedback().ShouldBe(feedback);
+        }
+
+        [Fact]
+        public void Successful_public_not_anonymous_feedback()
+        {
+            string feedback = "Awesome!";
+            postFeedbackPage.InsertFeedback(feedback);
+            postFeedbackPage.SelectPublicRadioButton();
+            postFeedbackPage.SelectNotAnonymousRadioButton();
+            postFeedbackPage.SubmitFeedback();
+
+            feedbackPublishListPage.Navigate();
+            feedbackPublishListPage.EnsurePageIsDisplayed();
+
+            feedbackCount.ShouldBe(feedbackPublishListPage.FeedbackCount() - 1);
+            feedbackPublishListPage.GetLastRowFeedback().ShouldBe(feedback);
+        }
+
+        [Fact]
+        public void Successful_private_not_anonymous_feedback()
+        {
+            string feedback = "Awesome!";
+            postFeedbackPage.InsertFeedback(feedback);
+            postFeedbackPage.SelectPrivateRadioButton();
+            postFeedbackPage.SelectNotAnonymousRadioButton();
+            postFeedbackPage.SubmitFeedback();
+
+            feedbackPublishListPage.Navigate();
+            feedbackPublishListPage.EnsurePageIsDisplayed();
+
+            feedbackCount.ShouldBe(feedbackPublishListPage.FeedbackCount() - 1);
+            feedbackPublishListPage.GetLastRowFeedback().ShouldBe(feedback);
+        }
+
+        [Fact]
+        public void Unsuccessful_feedback_empty_text_input_field()
+        {
+            string feedback = "";
+            postFeedbackPage.InsertFeedback(feedback);
+            postFeedbackPage.SelectPrivateRadioButton();
+            postFeedbackPage.SelectNotAnonymousRadioButton();
+
+            postFeedbackPage.SubmitButtonEnabled().ShouldBe(false);
+
+            postFeedbackPage.SubmitFeedback();
+
+            feedbackPublishListPage.Navigate();
+            feedbackPublishListPage.EnsurePageIsDisplayed();
+
+            feedbackCount.ShouldBe(feedbackPublishListPage.FeedbackCount());
+        }
+
+        [Fact]
+        public void Unsuccessful_feedback_visibility_radio_button_group_not_selected()
+        {
+            string feedback = "Awesome!";
+            postFeedbackPage.InsertFeedback(feedback);
+            postFeedbackPage.SelectNotAnonymousRadioButton();
+
+            postFeedbackPage.SubmitButtonEnabled().ShouldBe(false);
+
+            postFeedbackPage.SubmitFeedback();
+
+            feedbackPublishListPage.Navigate();
+            feedbackPublishListPage.EnsurePageIsDisplayed();
+
+            feedbackCount.ShouldBe(feedbackPublishListPage.FeedbackCount());
+        }
+
+        [Fact]
+        public void Unsuccessful_feedback_anonymity_radio_button_group_not_selected()
+        {
+            string feedback = "Awesome!";
+            postFeedbackPage.InsertFeedback(feedback);
+            postFeedbackPage.SelectPrivateRadioButton();
+
+            postFeedbackPage.SubmitButtonEnabled().ShouldBe(false);
+
+            postFeedbackPage.SubmitFeedback();
+
+            feedbackPublishListPage.Navigate();
+            feedbackPublishListPage.EnsurePageIsDisplayed();
+
+            feedbackCount.ShouldBe(feedbackPublishListPage.FeedbackCount());
+        }
+
+        public void Dispose()
+        {
+            driver.Quit();
+            driver.Dispose();
+        }
+
+        private void InitializeDriver()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("start-maximized");
+            options.AddArguments("disable-infobars");
+            options.AddArguments("--disable-extensions");
+            options.AddArguments("--disable-gpu");
+            options.AddArguments("--disable-dev-shm-usage");
+            options.AddArguments("--no-sandbox");
+            options.AddArguments("--disable-notifications");
+            driver = new ChromeDriver(options);
+        }
+    }
+}
