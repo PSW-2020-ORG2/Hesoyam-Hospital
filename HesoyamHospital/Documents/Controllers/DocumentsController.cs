75f28fc4e254e0cce4e90246f02826d143da07f3
Milijana <milijana.djordjevic1998@gmail.com>
2020-12-25 19:55:17 +0100

    refactor: controllers in documents microservice

---
+++ HesoyamHospital/Documents/Controllers/DocumentsController.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using Backend.Model.PatientModel;
+using Backend.Util;
+using Documents.Mappers;
+using Documents.Service.Abstract;
+using Documents.Validation;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Documents.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DocumentController : ControllerBase
+    {
+        private readonly IDocumentService _documentService;
+        private readonly DocumentsValidation _validation;
+
+        public DocumentController(IDocumentService documentService)
+        {
+            _documentService = documentService;
+            _validation = new DocumentsValidation();
+        }
+
+        [HttpPost("simple-search/{id}")]
+        public IActionResult SimpleSearchDocs([FromBody] DocumentSearchCriteria criteria, long id)
+        {
+            if (!_validation.IsSearchCriteriaValid(criteria)) return BadRequest();
+
+            return Ok(DocumentsMapper.DocumentToDocumentDTO(_documentService.SimpleSearchDocs(criteria, id).ToList()));
+        }
+
+        [HttpPost("advanced-search/{id}")]
+        public IActionResult AdvanceSearchDocs([FromBody] AdvancedDocumentSearchCriteria criteria, long id)
+        {
+            if (!_validation.IsAdvancedSearchCriteriaValid(criteria)) return BadRequest();
+
+            return Ok(DocumentsMapper.DocumentToDocumentDTO(_documentService.AdvanceSearchDocs(criteria, id).ToList()));
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            return Ok(DocumentsMapper.DocumentToDocumentDTO(_documentService.GetAllByPatient(id).ToList()));
+        }
+    }
+}
