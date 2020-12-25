cf3db84f3b01222935c60aef14a0297437f4be94
Milijana <milijana.djordjevic1998@gmail.com>
2020-12-25 19:59:14 +0100

    refactor: documents microservice DTOs

---
+++ HesoyamHospital/Documents/DTOs/MedicalRecordDTO.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace Documents.DTOs
+{
+    public class MedicalRecordDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string Address { get; set; }
+        public string DateOfBirth { get; set; }
+        public string MedicalId{ get; set; }
+        public string PersonalId { get; set; }
+        public string MobilePhone { get; set; }
+        public string HomePhone { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string BloodType { get; set; }
+        public string SelectedDoctorName { get; set; }
+        public List<string> Alergies { get; set; }
+        public List<PrescriptionDTO> Prescriptions { get; set; }
+
+        public MedicalRecordDTO() { }
+        public MedicalRecordDTO(string name, string surname, string middleName, string address, string date, string medicalId, string personalId, string phone, string homePhone, string email, string username, string password, string bloodType, string selectedDoctorName, List<string> alergies, List<PrescriptionDTO> presc) {
+            FirstName = name;
+            LastName = surname;
+            MiddleName = middleName;
+            Address = address;
+            DateOfBirth = date;
+            MedicalId = medicalId;
+            PersonalId = personalId;
+            MobilePhone = phone;
+            HomePhone = homePhone;
+            Email = email;
+            Username = username;
+            Password = password;
+            BloodType = bloodType;
+            SelectedDoctorName = selectedDoctorName;
+            Alergies = alergies;
+            Prescriptions = presc;
+        }
+    }
+}
---
+++ HesoyamHospital/Documents/DTOs/SelectedDoctorDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace Documents.DTOs
+{
+    public class SelectedDoctorDTO
+    {
+        public long DoctorId { get; set; }
+        public string Username { get; set; }
+
+        public SelectedDoctorDTO() { }
+
+        public SelectedDoctorDTO(long id, string username)
+        {
+            DoctorId = id;
+            Username = username;
+        }
+    }
+}
