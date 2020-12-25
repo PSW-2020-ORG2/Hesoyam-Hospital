1a731cfaf2a506b9f5e2960b0f84c0ec7eebba74
Milijana <milijana.djordjevic1998@gmail.com>
2020-12-25 19:59:47 +0100

    refactor: feedback microservice DTOs

---
+++ HesoyamHospital/Feedback/DTOs/MeanDTO.cs
@@ -0,0 +1,22 @@
+﻿namespace Feedbacks.DTOs
+{
+    public class MeanDTO
+    {
+        public double AnswerOne { get; set; }
+        public double AnswerTwo { get; set; }
+        public double AnswerThree { get; set; }
+        public double AnswerFour { get; set; }
+
+        public MeanDTO(double answerOne, double answerTwo, double answerThree, double answerFour)
+        {
+
+            AnswerOne = answerOne;
+            AnswerTwo = answerTwo;
+            AnswerThree = answerThree;
+            AnswerFour = answerFour;
+
+        }
+        public MeanDTO() { }
+    }
+}
+
---
+++ HesoyamHospital/Feedback/DTOs/NewFeedbackDTO.cs
@@ -0,0 +1,20 @@
+﻿namespace Feedbacks.DTOs
+{
+    public class NewFeedbackDTO
+    {
+        public string Comment { get; set; }
+
+        public bool Anonymous { get; set; }
+
+        public bool Public { get; set; }
+
+        public NewFeedbackDTO() { }
+
+        public NewFeedbackDTO(string _comment, bool _anonymous, bool _public)
+        {
+            this.Comment = _comment;
+            this.Anonymous = _anonymous;
+            this.Public = _public;
+        }
+    }
+}
