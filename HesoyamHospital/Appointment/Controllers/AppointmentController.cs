fe99539865489ca2f26e533d53b922509635103c
Milijana <milijana.djordjevic1998@gmail.com>
2020-12-25 19:53:15 +0100

    refactor: controllers in appointment microservice

---
+++ HesoyamHospital/Appointment/Controllers/AppointmentController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Appointments.DTOs;
+using Appointments.Mappers;
+using Appointments.Service.Abstract;
+using Appointments.Validation;
+using Backend.Model.PatientModel;
+using Backend.Model.UserModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Appointments.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppointmentController : ControllerBase
+    {
+        private readonly IAppointmentService _appointmentService;
+        private readonly IPatientService _patientService;
+        private readonly long defaultPatientId = 500;
+        private readonly AppointmentValidation _appointmentValidation;
+
+        public AppointmentController(IAppointmentService appointmentService, IPatientService patientService)
+        {
+            _appointmentService = appointmentService;
+            _patientService = patientService;
+            _appointmentValidation = new AppointmentValidation();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetAllByPatient(long id)
+        {
+            if (id != defaultPatientId) return BadRequest();
+            return Ok(AppointmentMapper.AppointmentToAppointmentForObservationDto(_appointmentService.GetAllByPatient(id).ToList()));
+        }
+
+        [HttpPut("cancel")]
+        public IActionResult Cancel([FromBody] long id)
+        {
+            Appointment appointment = _appointmentService.GetByID(id);
+            if (appointment == null) return NotFound();
+            if (!_appointmentValidation.IsPossibleToCancelAppointment(appointment, defaultPatientId)) return BadRequest();
+            _appointmentService.Cancel(defaultPatientId, id);
+            return Ok();
+        }
+
+        [HttpGet("getSuspiciousPatients")]
+        public IActionResult GetSuspiciousPatients()
+        {
+            List<BlockPatientDTO> suspiciousPatients = _appointmentService.GetSuspiciousPatients();
+            if (suspiciousPatients == null || suspiciousPatients.Count == 0) return NotFound();
+            return Ok(suspiciousPatients);
+        }
+
+        [HttpPut("block/{username}")]
+        public IActionResult BlockPatient(string username)
+        {
+            Patient patient = _patientService.GetByUsername(username);
+            if (patient == null) return BadRequest();
+            _appointmentService.BlockPatient(patient);
+            return Ok();
+        }
+    }
+}
---
+++ HesoyamHospital/Appointment/Controllers/AppointmentSchedulingController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Appointments.DTOs;
+using Appointments.Mappers;
+using Appointments.Service.Abstract;
+using Backend.Model.UserModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Appointments.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppointmentSchedulingController : ControllerBase
+    {
+        private readonly IAppointmentSchedulingService _appointmentSchedulingService;
+        private readonly IPatientService _patientService;
+
+        public AppointmentSchedulingController(IAppointmentSchedulingService appointmentSchedulingService, IPatientService patientService)
+        {
+            _appointmentSchedulingService = appointmentSchedulingService;
+            _patientService = patientService;
+        }
+
+        [HttpGet("getDoctorsByType/{type}")]
+        public IActionResult GetDoctorsByType(string type)
+        {
+            List<Doctor> doctors = _appointmentSchedulingService.GetDoctorsByType(type);
+            if (doctors == null || doctors.Count == 0) return NotFound();
+            List<DoctorDTO> dtos = DoctorMapper.DoctorListToDTOList(doctors);
+            return Ok(dtos.ToArray());
+        }
+
+        [HttpPut("getTimesForDoctor")]
+        public IActionResult GetTimesForDoctor(DoctorDateDTO dto)
+        {
+            if (dto == null) return BadRequest();
+            List<DateTime> availableAppointments = _appointmentSchedulingService.GetTimesForDoctorAndDate(dto.Id, dto.Date).ToList();
+            if (availableAppointments == null || availableAppointments.Count == 0) return NotFound();
+            return Ok(IntervalMapper.DateTimesToIntervalDTOs(availableAppointments).ToArray());
+        }
+
+        [HttpPost("recommendation")]
+        public IActionResult GetTimesForDoctor(PriorityDTO dto)
+        {
+            if (dto == null) return BadRequest();
+            List<PriorityIntervalDTO> availableAppointments = _appointmentSchedulingService.GetRecommendedTimes(dto).ToList();
+            if (availableAppointments == null || availableAppointments.Count == 0) return NotFound();
+            return Ok(availableAppointments.ToArray());
+        }
+
+        [HttpGet("getTimesForSelectedDoctor/{id}")]
+        public IActionResult GetTimesForSelectedDoctor(long id)
+        {
+            Patient patient = _patientService.GetByID(id);
+            if (patient == null || patient.SelectedDoctor == null) return BadRequest();
+            List<DateTime> availableAppointments = _appointmentSchedulingService.GetTimesForSelectedDoctor(patient).ToList();
+            if (availableAppointments == null || availableAppointments.Count == 0) return NotFound();
+            return Ok(IntervalMapper.DateTimesToIntervalDTOs(availableAppointments).ToArray());
+        }
+
+        [HttpPost("saveAppointment")]
+        public IActionResult SaveAppointment(AppointmentDTO dto)
+        {
+            if (dto == null || dto.DoctorId == 0) return BadRequest();
+            if (_appointmentSchedulingService.MultipleAppoitments(dto)) return BadRequest("SCHEDULING FAILED");
+            _appointmentSchedulingService.SaveAppointment(AppointmentMapper.AppointmentDtoToAppointment(dto));
+            return Ok();
+        }
+
+        [HttpPost("saveSelectedDoctorAppointment")]
+        public IActionResult SaveSelecetdDoctorAppointment(AppointmentDTO dto)
+        {
+            if (dto == null || dto.PatientId == 0) return BadRequest();
+            Patient patient = _patientService.GetByID(dto.PatientId);
+            if (patient.SelectedDoctor == null) return NotFound();
+            dto.DoctorId = patient.SelectedDoctor.Id;
+            if (_appointmentSchedulingService.MultipleAppoitments(dto)) return BadRequest("SCHEDULING FAILED");
+            _appointmentSchedulingService.SaveAppointment(AppointmentMapper.AppointmentDtoToAppointment(dto));
+            return Ok();
+        }
+    }
+}
