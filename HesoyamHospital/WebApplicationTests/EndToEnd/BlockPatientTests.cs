d901713ba8f12a76aacc80b47984d62875d55a05
Milijana <milijana.djordjevic1998@gmail.com>
2020-12-26 08:03:06 +0100

    test: end to end page and test for block patient initialized

---
+++ HesoyamHospital/WebApplicationTests/EndToEnd/BlockPatientTests.cs
@@ -0,0 +1,49 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Shouldly;
+using System;
+using System.Threading;
+using WebApplicationTests.EndToEnd.Pages;
+using Xunit;
+
+namespace WebApplicationTests.EndToEnd
+{
+    public class BlockPatientTests : IDisposable
+    {
+        private IWebDriver driver;
+        private readonly BlockPatientList blockPatientList;
+        private readonly int blockPatientCount;
+
+        public BlockPatientTests()
+        {
+            InitializeDriver();
+            blockPatientList = new BlockPatientList(driver);
+            blockPatientList.Navigate();
+            Thread.Sleep(30000);
+            blockPatientList.URI.ShouldBeEquivalentTo(driver.Url);
+        }
+        public void Dispose()
+        {
+            driver.Quit();
+            driver.Dispose();
+        }
+
+        [Fact]
+        public void Successful_blocking()
+        {
+        }
+
+        private void InitializeDriver()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("start-maximized");
+            options.AddArguments("disable-infobars");
+            options.AddArguments("--disable-extensions");
+            options.AddArguments("--disable-gpu");
+            options.AddArguments("--disable-dev-shm-usage");
+            options.AddArguments("--no-sandbox");
+            options.AddArguments("--disable-notifications");
+            driver = new ChromeDriver(options);
+        }
+    }
+}
---
+++ HesoyamHospital/WebApplicationTests/EndToEnd/Pages/BlockPatientList.cs
@@ -0,0 +1,62 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.ObjectModel;
+
+namespace WebApplicationTests.EndToEnd.Pages
+{
+    class BlockPatientList
+    {
+        private readonly IWebDriver driver;
+        public string URI = "http://localhost:4200/feedback/admin/block-patients";
+
+        private ReadOnlyCollection<IWebElement> Rows => driver.FindElements(By.XPath("//table/tbody/tr"));
+        private IWebElement LastRowUsername => driver.FindElement(By.XPath("//table/tbody/tr[last()]/td[1]"));
+        private IWebElement LastRowFullName => driver.FindElement(By.XPath("//table/tbody/tr[last()]/td[2]"));
+        private IWebElement LastRowCancelCount => driver.FindElement(By.XPath("//table/tbody/tr[last()]/td[3]"));
+        private IWebElement LastRowButton => driver.FindElement(By.XPath("//table/tbody/tr[last()]/td[4]/button"));
+
+        public BlockPatientList(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void EnsurePageIsDisplayed()
+        {
+            var wait = new WebDriverWait(driver, new TimeSpan(0, 0, 30));
+            wait.Until(condition =>
+            {
+                try
+                {
+                    return Rows.Count > 0;
+                }
+                catch (StaleElementReferenceException)
+                {
+                    return false;
+                }
+                catch (NoSuchElementException)
+                {
+                    return false;
+                }
+            });
+        }
+
+        public int SuspicousPatientsCount()
+            => Rows.Count;
+
+        public string GetLastRowUsername()
+            => LastRowUsername.Text;
+
+        public string GetLastRowFullName()
+            => LastRowFullName.Text;
+
+        public string GetLastRowCancelCount()
+            => LastRowCancelCount.Text;
+
+        public void Navigate()
+            => driver.Navigate().GoToUrl(URI);
+
+        public void BlockPatient()
+          => LastRowButton.Click();
+    }
+}
