6768bcf4e1a95fc6307c061c2b549171519d5e9e
anja-meseldzic <aanja.meseldzic@gmail.com>
2020-12-27 17:39:50 +0100

    test: end to end test for canceling appointments

---
+++ HesoyamHospital/WebApplicationTests/EndToEnd/CancelAppointmentTests.cs
@@ -0,0 +1,52 @@
+﻿using OpenQA.Selenium;
+using System;
+using WebApplicationTests.EndToEnd.Pages;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using Shouldly;
+using Xunit;
+
+namespace WebApplicationTests.EndToEnd
+{
+    public class CancelAppointmentTests : IDisposable
+    {
+        private IWebDriver driver;
+        private readonly AppointmentsList appointmentsList;
+
+        public CancelAppointmentTests()
+        {
+            InitializeDriver();
+            appointmentsList = new AppointmentsList(driver);
+            appointmentsList.Navigate();
+            appointmentsList.EnsurePageIsDisplayed();
+            Thread.Sleep(5000);
+            appointmentsList.URI.ShouldBeEquivalentTo(driver.Url);
+        }
+
+        [Fact]
+        public void Successful_canceling()
+        {
+            appointmentsList.CancelAppointment();
+            Thread.Sleep(5000);
+            appointmentsList.CheckState().ShouldBe("CANCELLED");
+        }
+        public void Dispose()
+        {
+            driver.Quit();
+            driver.Dispose();
+        }
+      
+        private void InitializeDriver()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("start-maximized");
+            options.AddArguments("disable-infobars");
+            options.AddArguments("--disable-extensions");
+            options.AddArguments("--disable-gpu");
+            options.AddArguments("--disable-dev-shm-usage");
+            options.AddArguments("--no-sandbox");
+            options.AddArguments("--disable-notifications");
+            driver = new ChromeDriver(options);
+        }
+    }
+}
---
+++ HesoyamHospital/WebApplicationTests/EndToEnd/Pages/AppointmentsList.cs
@@ -0,0 +1,55 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace WebApplicationTests.EndToEnd.Pages
+{
+    class AppointmentsList
+    {
+        private readonly IWebDriver driver;
+        public string URI = "http://localhost:4200/medical-record";
+
+        private ReadOnlyCollection<IWebElement> Rows => driver.FindElements(By.XPath("//table[@id='appointmentsTable']/tbody/tr"));
+        private ReadOnlyCollection<IWebElement> Buttons => driver.FindElements(By.Id("cancelButton"));
+        private IWebElement FirstRowState => driver.FindElement(By.XPath("//table/tbody/tr/td[1]"));
+        private IWebElement FirstRowButton => driver.FindElement(By.XPath("//table/tbody/tr/td[8]"));
+
+        public AppointmentsList(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void EnsurePageIsDisplayed()
+        {
+            var wait = new WebDriverWait(driver, new TimeSpan(0, 0, 30));
+            wait.Until(condition =>
+            {
+                try
+                {
+                    return Rows.Count > 0;
+                }
+                catch (StaleElementReferenceException)
+                {
+                    return false;
+                }
+                catch (NoSuchElementException)
+                {
+                    return false;
+                }
+            });
+        }
+
+        public void Navigate()
+            => driver.Navigate().GoToUrl(URI);
+
+        public void CancelAppointment()
+          => FirstRowButton.Click();
+
+        public string CheckState()
+          => FirstRowState.Text;
+
+    }
+}
