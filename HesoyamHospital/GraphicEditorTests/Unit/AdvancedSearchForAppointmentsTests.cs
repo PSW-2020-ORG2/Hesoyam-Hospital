cd744d309c6b61e122e380298c26607f1f90f7af
milica1612 <57577588+milica1612@users.noreply.github.com>
2020-12-28 14:57:05 +0100

    refactor: test find available rooms

--- HesoyamHospital/GraphicEditorTests/Unit/AdvancedSearchForAppointmentsTests.cs
+++ HesoyamHospital/GraphicEditorTests/Unit/AdvancedSearchForAppointmentsTests.cs
@@ -1,5 +1,4 @@
 ﻿using Xunit;
-using System.Linq;
 using Shouldly;
 using Moq;
 using System;
@@ -9,8 +8,9 @@ using Backend.Repository.Abstract.HospitalManagementAbstractRepository;
 using Backend.Model.UserModel;
 using Backend.Repository.Abstract.MedicalAbstractRepository;
 using Backend.Model.PatientModel;
-using GraphicEditor;
 using Backend.Util;
+using Backend.Model.DoctorModel;
+using Backend.Model.UserModel;
 
 namespace GraphicEditorTests.Unit
 {
@@ -20,11 +20,20 @@ namespace GraphicEditorTests.Unit
         [Fact]
         public void Get_all_rooms()
         {
+            var rooms = new List<Room>();
+            Room room1 = new Room(1);
+            Room room2 = new Room(2);
+            Room room3 = new Room(3);
+            Room room4 = new Room(4);
+            rooms.Add(room1);
+            rooms.Add(room2);
+            rooms.Add(room3);
+            rooms.Add(room4);
             var stubRoomRepository = CreateStubRoomRepository();
             var stubAppointmentRepository = CreateStubAppointmentRepository();
             RoomService roomService = new RoomService(stubRoomRepository.Object, stubAppointmentRepository.Object);
             List<Room> allRooms = (List<Room>)roomService.GetAll();
-            allRooms.ShouldNotBeEmpty();
+            allRooms.ShouldBe(rooms);
         }
 
         [Fact]
@@ -32,9 +41,15 @@ namespace GraphicEditorTests.Unit
         {
             var stubRoomRepository = CreateStubRoomRepository();
             var stubAppointmentRepository = CreateStubAppointmentRepository();
+            var rooms = new List<Room>();
+            Room room3 = new Room(3);
+            Room room4 = new Room(4);
+
+            rooms.Add(room3);
+            rooms.Add(room4);
             RoomService roomService = new RoomService(stubRoomRepository.Object, stubAppointmentRepository.Object);
             List<Room> availableRooms = (List<Room>)roomService.GetAvailableRoomsByDate(new TimeInterval(new DateTime(2021, 1, 1,8,30,0), new DateTime(2021, 1, 1,9,0,0)));
-            availableRooms.ShouldNotBeEmpty();
+            availableRooms.ShouldBe(rooms);
 
         }
 
@@ -56,13 +71,23 @@ namespace GraphicEditorTests.Unit
 
         private Mock<IAppointmentRepository> CreateStubAppointmentRepository()
         {
+           // long id, Doctor doctor, Patient patient, Room room, AppointmentType appointmentType, TimeInterval timeInterval
             var stubAppointmentRepository = new Mock<IAppointmentRepository>();
             List<Appointment> appointments = new List<Appointment>();
-            Appointment appointment1 = new Appointment();
-            Appointment appointment2 = new Appointment();
-            Appointment appointment3 = new Appointment();
-            Appointment appointment4 = new Appointment();
-            stubAppointmentRepository.Setup(m => m.GetAppointmentsByTime(new TimeInterval(new DateTime(2021,1,1), new DateTime(2021,2,2)))).Returns(appointments);
+           
+            List<Shift> shifts = new List<Shift>();
+            Doctor doctor = new Doctor("sanja", "sanja123", new DateTime(2016,10,10), "Sanja", "Vukovic", "", "1010972100102",Sex.FEMALE, new DateTime(1977,10,10), null, null,"","064/221-58-40",null, null, new TimeTable(1, shifts), null, null, DoctorType.GENERAL_PRACTITIONER);
+            Patient patient = new Patient("ana", "ana123", new DateTime(2016, 10, 10), "Ana", "Markovic", "", "1111991715295", Sex.FEMALE, new DateTime(1991, 11, 11), null, null, "", "066/112-22-22", null, null, doctor,"1234156117");
+            Room room1 = new Room(1);
+            Room room2 = new Room(2);
+            Room room3 = new Room(3);
+            Room room4 = new Room(4);
+            TimeInterval timeInterval1 = new TimeInterval(new DateTime(2021, 1, 1, 8, 30, 0), new DateTime(2021, 1, 1, 9, 0, 0));
+            Appointment appointment1 = new Appointment(1, doctor, patient, room1, AppointmentType.checkup, timeInterval1);
+            Appointment appointment2 = new Appointment(2, doctor, patient, room2, AppointmentType.checkup, timeInterval1);
+            appointments.Add(appointment1);
+            appointments.Add(appointment2);
+            stubAppointmentRepository.Setup(m => m.GetAppointmentsByTime(new TimeInterval(new DateTime(2021,1,1,8,30,0), new DateTime(2021,1,1,9,0,0)))).Returns(appointments);
             return stubAppointmentRepository;
         }
     }
