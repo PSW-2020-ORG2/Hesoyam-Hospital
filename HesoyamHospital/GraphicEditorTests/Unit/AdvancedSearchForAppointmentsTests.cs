17556990404a550d88383abb8d37fd7ca74dfcf8
milica1612 <57577588+milica1612@users.noreply.github.com>
2020-12-28 01:14:07 +0100

    test: get available rooms by date

--- HesoyamHospital/GraphicEditorTests/Unit/AdvancedSearchForAppointmentsTests.cs
+++ HesoyamHospital/GraphicEditorTests/Unit/AdvancedSearchForAppointmentsTests.cs
@@ -8,6 +8,9 @@ using Backend.Service.HospitalManagementService;
 using Backend.Repository.Abstract.HospitalManagementAbstractRepository;
 using Backend.Model.UserModel;
 using Backend.Repository.Abstract.MedicalAbstractRepository;
+using Backend.Model.PatientModel;
+using GraphicEditor;
+using Backend.Util;
 
 namespace GraphicEditorTests.Unit
 {
@@ -17,11 +20,22 @@ namespace GraphicEditorTests.Unit
         [Fact]
         public void Get_all_rooms()
         {
-            var stubAppointmentRepository = new Mock<IAppointmentRepository>();
             var stubRoomRepository = CreateStubRoomRepository();
+            var stubAppointmentRepository = CreateStubAppointmentRepository();
             RoomService roomService = new RoomService(stubRoomRepository.Object, stubAppointmentRepository.Object);
             List<Room> allRooms = (List<Room>)roomService.GetAll();
             allRooms.ShouldNotBeEmpty();
+        }
+
+        [Fact]
+        public void Get_available_rooms_by_date()
+        {
+            var stubRoomRepository = CreateStubRoomRepository();
+            var stubAppointmentRepository = CreateStubAppointmentRepository();
+            RoomService roomService = new RoomService(stubRoomRepository.Object, stubAppointmentRepository.Object);
+            List<Room> availableRooms = (List<Room>)roomService.GetAvailableRoomsByDate(new TimeInterval(new DateTime(2021, 1, 1,8,30,0), new DateTime(2021, 1, 1,9,0,0)));
+            availableRooms.ShouldNotBeEmpty();
+
         }
 
         private Mock<IRoomRepository> CreateStubRoomRepository()
@@ -38,6 +52,18 @@ namespace GraphicEditorTests.Unit
             rooms.Add(room4);
             stubRoomRepository.Setup(m => m.GetAll()).Returns(rooms);
             return stubRoomRepository;
+        }
+
+        private Mock<IAppointmentRepository> CreateStubAppointmentRepository()
+        {
+            var stubAppointmentRepository = new Mock<IAppointmentRepository>();
+            List<Appointment> appointments = new List<Appointment>();
+            Appointment appointment1 = new Appointment();
+            Appointment appointment2 = new Appointment();
+            Appointment appointment3 = new Appointment();
+            Appointment appointment4 = new Appointment();
+            stubAppointmentRepository.Setup(m => m.GetAppointmentsByTime(new TimeInterval(new DateTime(2021,1,1), new DateTime(2021,2,2)))).Returns(appointments);
+            return stubAppointmentRepository;
         }
     }
 }
