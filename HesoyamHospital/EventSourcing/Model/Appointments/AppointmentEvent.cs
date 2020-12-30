4eb87ef8b65a86d71cb73f1f229a7f816540775c
Nikola Dragic <nikola_dragic@hotmail.com>
2020-12-30 10:52:09 +0100

    feat: Initial Event Source project

---
+++ HesoyamHospital/EventSourcing/Model/Appointments/AppointmentEvent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventSourcing.Model.Appointments
+{
+    public class AppointmentEvent : Event
+    {
+        public string PatientID { get; set; }
+        public string DoctorID { get; set; }
+
+        public AppointmentType AppointmentType { get; set; }
+
+        public AppointmentEvent(DateTime timestamp, string patientID, string doctorID, AppointmentType appointmentType) : base(timestamp)
+        {
+            PatientID = patientID;
+            DoctorID = doctorID;
+            AppointmentType = appointmentType;
+        }
+
+
+    }
+}
---
+++ HesoyamHospital/EventSourcing/Model/Event.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventSourcing.Model
+{
+    public abstract class Event
+    {
+        [System.ComponentModel.DataAnnotations.Schema.DatabaseGenerat‌ed(System.ComponentM‌​odel.DataAnnotations‌​.Schema.DatabaseGeneratedOp‌​tion.None)]
+        public int Id { get; set; }
+        public DateTime Timestamp;
+        
+
+        public Event(DateTime timestamp)
+        {
+            Timestamp = timestamp;
+        }
+
+
+    }
+}
