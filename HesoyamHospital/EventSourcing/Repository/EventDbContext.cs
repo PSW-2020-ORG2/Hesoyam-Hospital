6d90bb3af7f09e5d56a0b84d138c925af796350d
Nikola Dragic <nikola_dragic@hotmail.com>
2021-01-06 12:24:39 +0100

    feat: Event DB context

---
+++ HesoyamHospital/EventSourcing/Repository/EventDbContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+using EventSourcing.Model;
+using EventSourcing.Model.Appointments;
+
+namespace EventSourcing.Repository
+{
+    public class EventDbContext : DbContext
+    {
+        public DbSet<AppointmentEvent> AppointmentEvents { get; set; }
+
+        public EventDbContext(DbContextOptions<EventDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AppointmentEvent>().ToTable("AppointmentEvents");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseLazyLoadingProxies().UseMySql(Environment.GetEnvironmentVariable("MyDbConnectionString"));
+            }
+                
+        }
+    }
+}
