b2db0fc4b7055017bea60611c6653847c369456e
Nikola Dragic <nikola_dragic@hotmail.com>
2021-01-06 12:15:42 +0100

    feat: Updated appointment event model

--- HesoyamHospital/EventSourcing/Model/Appointments/AppointmentEvent.cs
+++ HesoyamHospital/EventSourcing/Model/Appointments/AppointmentEvent.cs
@@ -1,14 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace EventSourcing.Model.Appointments
 {
-    public class AppointmentEvent : Event
+    public class AppointmentEvent
     {
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity), Key()]
+        public int Id { get; set; }
+
+        public DateTime Timestamp { get; set; }
+
         public string PatientID { get; set; }
         public string DoctorID { get; set; }
+
+
 
         public AppointmentType AppointmentType { get; set; }
 
@@ -17,12 +27,17 @@ namespace EventSourcing.Model.Appointments
 
         }
 
-        public AppointmentEvent(DateTime timestamp, string patientID, string doctorID, AppointmentType appointmentType) : base(timestamp)
+        public AppointmentEvent(DateTime timestamp, string patientID, string doctorID, AppointmentType appointmentType)
         {
             PatientID = patientID;
             DoctorID = doctorID;
             AppointmentType = appointmentType;
+            Timestamp = timestamp;
         }
+
+
+
+
 
 
     }
