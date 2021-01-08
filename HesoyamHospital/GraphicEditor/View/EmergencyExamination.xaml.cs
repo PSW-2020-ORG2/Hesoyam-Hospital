3b681fd9c321e3e79570b97882e2adc14dc5173f
ivanagavric98 <gavric.ra160.2017@uns.ac.rs>
2021-01-08 20:34:50 +0100

    feat: schedule an emergency examination wpf

---
+++ HesoyamHospital/GraphicEditor/View/EmergencyExamination.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GraphicEditor.View
+{
+    /// <summary>
+    /// Interaction logic for EmergencyExamination.xaml
+    /// </summary>
+    public partial class EmergencyExamination : Window
+    {
+        public EmergencyExamination()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonScheduleEmergencyAppointment_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
