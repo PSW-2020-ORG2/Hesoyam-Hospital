cdf6c745bef7b50de2c9e230a00b596c61bbf743
zorana98 <lazic.ra53.2017@uns.ac.rs>
2021-01-08 23:08:59 +0100

    feat: Added combo box items

--- HesoyamHospital/GraphicEditor/View/EmergencyExamination.xaml.cs
+++ HesoyamHospital/GraphicEditor/View/EmergencyExamination.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Backend.Model.ManagerModel;
+using Backend.Model.UserModel;
+using Backend.Service.HospitalManagementService;
+using Backend.Service.UsersService;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -17,13 +21,50 @@ namespace GraphicEditor.View
     /// </summary>
     public partial class EmergencyExamination : Window
     {
+        private readonly InventoryService inventoryService;
+        private UserService userService = Backend.AppResources.getInstance().userService;
+
         public EmergencyExamination()
         {
             InitializeComponent();
+            inventoryService = Backend.AppResources.getInstance().inventoryService;
+            userService = Backend.AppResources.getInstance().userService;
+            List<InventoryItem> inventories = (List<InventoryItem>)inventoryService.GetInventoryItems();
+           
+            foreach (InventoryItem inventoryItem in inventories)
+            {
+                ComboBoxItem item = new ComboBoxItem();
+                item.Tag = inventoryItem;
+                item.Content = inventoryItem.Name;
+                chooseEquipment.Items.Add(item);
+            }
         }
 
         private void buttonScheduleEmergencyAppointment_Click(object sender, RoutedEventArgs e)
-        {
+        { 
+            List<User> users = (List<User>)userService.GetAll();
+
+            string name = Name.Text;
+            string surname = Surname.Text;
+            string jmbg = JMBG.Text;
+
+            /*User user = new User(name, jmbg, name, surname, null, jmbg, Sex.OTHER, new DateTime(2021,01,01), null, null, null, null, null);   
+            
+            foreach(User user1 in users)
+            {
+                if(jmbg != user1.Jmbg)
+                {
+                    userService.Create(user);
+                }
+                else
+                {
+                    user = user1;
+                }
+            }
+            */
+           
+            //User loggedIn = Backend.AppResources.getInstance().loggedInUser;
+
 
         }
     }
