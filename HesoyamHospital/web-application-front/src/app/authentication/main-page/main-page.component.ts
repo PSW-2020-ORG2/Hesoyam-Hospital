2fef9064ab2dbd207cc09f456be1fb0f023672c2
Milijana <milijana.djordjevic1998@gmail.com>
2021-01-08 21:26:23 +0100

    feat: frontend pharmacy names

--- HesoyamHospital/web-application-front/src/app/authentication/main-page/main-page.component.ts
+++ HesoyamHospital/web-application-front/src/app/authentication/main-page/main-page.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../service/authentication.service';
 
@@ -9,40 +10,43 @@ import { AuthenticationService } from '../service/authentication.service';
 })
 export class MainPageComponent implements OnInit {
 
-  imageObject: Array<object> = [{
+  imageObject: Array<object> = [];
+  titles : string[] = [ 'Pharmacy 1', 'Pharmacy 2', 'Pharmacy 3', 'Pharmacy 4', 'Pharmacy 5'];
+
+  constructor(public _authService : AuthenticationService, private _http : HttpClient) { }
+
+  ngOnInit(): void {
+
+    this.imageObject = [{
       image: '../../assets/01.jpg',
       thumbImage: '../../assets/01.jpg',
       alt: 'alt of image',
-      title: 'Image 1'
+      title: this.titles[0]
     }, 
     {
       image: 'assets/02.jpg',
       thumbImage: 'assets/02.jpg',
       alt: 'alt of image',
-      title: 'Image 2'
+      title: this.titles[1]
     },
     {
       image: 'assets/03.jpg',
       thumbImage: 'assets/03.jpg',
       alt: 'alt of image',
-      title: 'Image 2'
+      title: this.titles[2]
     },
     {
       image: 'assets/04.jpg',
       thumbImage: 'assets/04.jpg',
       alt: 'alt of image',
-      title: 'Image 2'
+      title: this.titles[3]
     },
     {
       image: 'assets/05.jpg',
       thumbImage: 'assets/05.jpg',
       alt: 'alt of image',
-      title: 'Image 2'
-    }
-  ];
-  constructor(public _authService : AuthenticationService) { }
-
-  ngOnInit(): void {
+      title: this.titles[4]
+    }];
   }
 
 }
