566da6de54f86b434388a2106ef03bf903bda286
zorana98 <lazic.ra53.2017@uns.ac.rs>
2021-01-08 01:06:14 +0100

    refactor: Show inventory items in datagrid

--- HesoyamHospital/GraphicEditor/View/EquipmentForSpecialistAppointment.xaml.cs
+++ HesoyamHospital/GraphicEditor/View/EquipmentForSpecialistAppointment.xaml.cs
@@ -15,36 +15,17 @@ namespace GraphicEditor.View
     {
         private InventoryService inventoryService;
         private List<InventoryItem> inventoryItems;
-        private List<CheckBoxDTO> checkBoxDTOs;
+        private List<InventoryNameDTO> inventoryNamesDTOs;
+
+        //private List<CheckBoxDTO> checkBoxDTOs;
 
         public EquipmentForSpecialistAppointment()
         {
-            InitializeComponent();
-            inventoryService = Backend.AppResources.getInstance().inventoryService;
+            InitializeComponent();  
+            inventoryService = Backend.AppResources.getInstance().inventoryService;  
             inventoryItems = (List<InventoryItem>)inventoryService.GetInventoryItems();
-            List<CheckBox> checkBoxList = new List<CheckBox>();
-            
-
-            CheckBox checkBox = new CheckBox();
-            
-            
-            foreach (InventoryItem inventoryItem in inventoryItems)
-            {
-                checkBox.Tag = inventoryItem;
-                checkBox.Content = inventoryItem.Name;
-                checkBox.IsChecked = false;
-                checkBoxList.Add(checkBox);
-            }
-            equipmentDataGrid.ItemsSource = checkBoxList;
-        }
-        private List<CheckBoxDTO> ConvertFromCheckBoxToDTO(List<CheckBox> checkBoxes)
-        {
-            List<CheckBoxDTO> result = new List<CheckBoxDTO>();
-
-            foreach (CheckBox check in checkBoxes)
-                result.Add(new CheckBoxDTO(check.Name));
-
-            return result;
+            inventoryNamesDTOs = InvertoryItemMapper.ConvertFromIventoryItemNameToDTO(inventoryItems);
+            equipmentDataGrid.ItemsSource = inventoryNamesDTOs;
         }
     }
 }
