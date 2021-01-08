e48abb81027621cd30a55234e8602495916149e8
LukaKrickovic <krickovicluka@gmail.com>
2021-01-08 11:06:24 +0100

    feature: Patient events implemented

---
+++ HesoyamHospital/EventSourceClasses/Authentication/BlockPatientEvent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventSourceClasses.Authentication
+{
+    class BlockPatientEvent
+    {
+        public long Id { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string Username { get; set; }
+
+        public BlockPatientEvent() { }
+        public BlockPatientEvent(string username) 
+        {
+            Username = username;
+        }
+        public BlockPatientEvent(string username, DateTime timestamp) 
+        {
+            Username = username;
+            Timestamp = timestamp;
+        }
+    }
+}
---
+++ HesoyamHospital/EventSourceClasses/Authentication/SelectedDoctorEvent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventSourceClasses.Authentication
+{
+    public class SelectedDoctorEvent
+    {
+        public long DoctorId { get; set; }
+        public string Username { get; set; }
+
+        public long Id { get; set; }
+        public DateTime Timestamp { get; set; }
+
+        public SelectedDoctorEvent() { }
+
+        public SelectedDoctorEvent(long doctorID, string username) 
+        {
+            DoctorId = doctorID;
+            Username = username;
+        }
+        public SelectedDoctorEvent(long doctorID, string username, DateTime timestamp) 
+        {
+            DoctorId = doctorID;
+            Username = username;
+            Timestamp = timestamp;
+        }
+    }
+}
