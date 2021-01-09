d7463731340eeb68e3d9e9d6f91493cd883dc6c4
ivanagavric98 <gavric.ra160.2017@uns.ac.rs>
2021-01-09 01:30:06 +0100

    feat: load equipment and rooms into comboboxes

--- HesoyamHospital/GraphicEditor/View/EquipmentRelocation.xaml.cs
+++ HesoyamHospital/GraphicEditor/View/EquipmentRelocation.xaml.cs
@@ -9,6 +9,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Backend.Model.ManagerModel;
+using Backend.Model.UserModel;
+using Backend.Service.HospitalManagementService;
 
 namespace GraphicEditor.View
 {
@@ -17,9 +20,33 @@ namespace GraphicEditor.View
     /// </summary>
     public partial class EquipmentRelocation : Window
     {
+        private readonly InventoryService inventoryService;
+        private readonly RoomService roomService;
+
         public EquipmentRelocation()
         {
             InitializeComponent();
+            inventoryService = Backend.AppResources.getInstance().inventoryService;
+            List<InventoryItem> inventories = (List<InventoryItem>)inventoryService.GetInventoryItems();
+            roomService = Backend.AppResources.getInstance().roomService;
+            List<Room> rooms = (List<Room>)roomService.GetAll();
+
+            foreach (InventoryItem inventoryItem in inventories)
+            {
+                ComboBoxItem item = new ComboBoxItem();
+                item.Tag = inventoryItem;
+                item.Content = inventoryItem.Name + " - " + "Room location:" + " " + inventoryItem.RoomID;
+                chooseEquipment.Items.Add(item);
+            }
+
+            foreach (Room room in rooms)
+            {
+                ComboBoxItem item = new ComboBoxItem();
+                item.Tag = room;
+                item.Content = room.Id;
+                chooseDestinationRoom.Items.Add(item);
+            }
+
         }
         private void ToDate_KeyUp(object sender, KeyEventArgs e)
         {
