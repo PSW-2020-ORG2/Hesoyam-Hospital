6199eaee5ca5736d3f690e6a1007b874318f1f96
Milijana <milijana.djordjevic1998@gmail.com>
2021-01-15 17:04:23 +0100

    feat: get all patients in authentication microservice

---
+++ HesoyamHospital/Authentication/DTOs/PatientDTO.cs
@@ -0,0 +1,22 @@
+﻿namespace Authentication.DTOs
+{
+    public class PatientDTO
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Jmbg { get; set; }
+        public long PatientId { get; set; }
+
+        public PatientDTO()
+        {
+        }
+
+        public PatientDTO(string name, string surname, string jmbg, long patientId) 
+        {
+            Name = name;
+            Surname = surname;
+            Jmbg = jmbg;
+            PatientId = patientId;
+        }
+    }
+}
---
+++ HesoyamHospital/Authentication/Mappers/PatientMapper.cs
@@ -0,0 +1,22 @@
+﻿using Authentication.DTOs;
+using Authentication.Model;
+using System.Collections.Generic;
+
+namespace Authentication.Mappers
+{
+    public static class PatientMapper
+    {
+        public static PatientDTO PatientToPatientDTO(Patient patient)
+            =>new PatientDTO(patient.Name, patient.Surname, patient.Jmbg, patient.Id);
+
+        public static List<PatientDTO> PatientsToPatientDTOs(List<Patient> patients)
+        {
+            List<PatientDTO> dtos = new List<PatientDTO>();
+            foreach (Patient patient in patients)
+            {
+                dtos.Add(PatientToPatientDTO(patient));
+            }
+            return dtos;
+        }
+    }
+}
