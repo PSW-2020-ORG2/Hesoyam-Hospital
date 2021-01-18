c17bfce727078c6c76c8b91178ede3f2a0f37b83
Kristijan Pil <kristijanpil@gmail.com>
2021-01-18 17:39:46 +0100

    fix: Fixed hardcoded database connection string

--- HesoyamHospital/PharmacyRegistration/Repository/SQLRepository/Base/MyDbContext.cs
+++ HesoyamHospital/PharmacyRegistration/Repository/SQLRepository/Base/MyDbContext.cs
@@ -37,12 +37,7 @@ namespace PharmacyRegistration.Repository.SQLRepository.Base
         private string GenerateConnectionString()
         {
             string server = Environment.GetEnvironmentVariable("DATABASE_HOST") ?? "localhost";
-
-            // if (Environment.GetEnvironmentVariable("MyDbConnectionString") == null)
-            //     return "server=localhost;port=3306;database=mydb1;user=root;password=root";
-
-            //return "server=" + server.Trim() + ";" + Environment.GetEnvironmentVariable("MyDbConnectionString");
-            return "server=localhost;port=3306;database=mydb1;user=root;password=root";
+            return "server=" + server.Trim() + ";" + Environment.GetEnvironmentVariable("MyDbConnectionString");
         }
         private string GeneratePostgresConnectionString()
         {
