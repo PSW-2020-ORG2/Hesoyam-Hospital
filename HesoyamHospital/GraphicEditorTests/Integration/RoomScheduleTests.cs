ca4c8a3aeaca8b470976c40ef1fdd5af2d914098
milica1612 <57577588+milica1612@users.noreply.github.com>
2021-01-18 20:05:23 +0100

    test: get appointment by room

---
+++ HesoyamHospital/GraphicEditorTests/Integration/RoomScheduleTests.cs
@@ -0,0 +1,30 @@
+﻿using Backend.Model.DoctorModel;
+using Backend.Model.PatientModel;
+using Backend.Model.UserModel;
+using Backend.Repository.Abstract.MedicalAbstractRepository;
+using Backend.Service.MedicalService;
+using Backend.Util;
+using GraphicEditor;
+using Moq;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace GraphicEditorTests.Unit
+{
+    public class RoomScheduleTests
+    {
+        [Fact]
+        public void Get_appointments_by_room()
+        {
+            AppointmentService appointmentService = Backend.AppResources.getInstance().appointmentService;
+            Room room = new Room(101);
+            List<Appointment> appointments = (List<Appointment>)appointmentService.GetAppointmentsByRoom(room);
+            appointments.ShouldNotBeNull();
+
+        }
+
+    }
+}
