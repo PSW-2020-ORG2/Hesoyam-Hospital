f76a44ac9cd62741b259175bb47bafdad22ca5b4
lukapetrovic3110 <luka.petrovic@uns.ac.rs>
2021-01-25 22:43:08 +0100

    Revert "Revert "added window for schedule a basic renovation""
    This reverts commit 002bca0414d45cc2ec1eff9781c10922e06406f7.

---
+++ HesoyamHospital/GraphicEditor/View/BasicRenovation.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GraphicEditor.View
+{
+    /// <summary>
+    /// Interaction logic for BasicRenovation.xaml
+    /// </summary>
+    public partial class BasicRenovation : Window
+    {
+        public BasicRenovation()
+        {
+            InitializeComponent();
+        }
+
+        private void startDatePicker_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.F1)
+            {
+                startDatePicker.IsDropDownOpen = true;
+            }
+        }
+
+        private void endDatePicker_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.F1)
+            {
+                endDatePicker.IsDropDownOpen = true;
+            }
+        }
+        private void ButtonScheduleBasicRenovation_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
